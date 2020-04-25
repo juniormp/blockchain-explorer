a3a16f80c9cc3c22ed166a68a1ee767f39e79682
Maurício Junior <junior9771@gmail.com>
2020-04-25 17:22:19 -0300

    create dtos

---
+++ app/Domain/Explorer/TimestampDTO.php
@@ -0,0 +1,52 @@
+<?php
+
+
+namespace App\Explorer\Domain;
+
+
+use Facade\FlareClient\Time\Time;
+
+class TimestampDTO
+{
+    /** @var integer */
+    private $epoch;
+
+    /** @var integer */
+    private $unix;
+
+    /** @var string */
+    private $human;
+
+    public function getEpoch(): int
+    {
+        return $this->epoch;
+    }
+
+    public function setEpoch(int $epoch): TimestampDTO
+    {
+        $this->epoch = $epoch;
+        return $this;
+    }
+
+    public function getUnix(): int
+    {
+        return $this->unix;
+    }
+
+    public function setUnix(int $unix): TimestampDTO
+    {
+        $this->unix = $unix;
+        return $this;
+    }
+
+    public function getHuman(): string
+    {
+        return $this->human;
+    }
+
+    public function setHuman(string $human): TimestampDTO
+    {
+        $this->human = $human;
+        return $this;
+    }
+}
