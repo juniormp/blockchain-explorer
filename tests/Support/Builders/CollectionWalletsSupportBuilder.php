382b746a6e5fc2cf32a2767d2f58b4b4ce311a27
Maurício Junior <junior9771@gmail.com>
2020-04-27 08:41:18 -0300

    create wallets collection factory

--- tests/Support/Builders/CollectionWalletsSupportBuilder.php
+++ tests/Support/Builders/CollectionWalletsSupportBuilder.php
@@ -17,23 +17,25 @@ class CollectionWalletsSupportBuilder
         $collections = new CollectionsWalletDTO();
 
         return $collections
+            ->setCount(100)
+            ->setPageCount(40366)
             ->setTotalCount(4036503)
             ->setNext("/transactions?transform=true&page=2&limit=100")
             ->setPrevious("")
             ->setSelf("/transactions?transform=true&page=1&limit=100")
             ->setFirst("/transactions?transform=true&page=1&limit=100")
             ->setLast("/transactions?transform=true&page=40366&limit=100")
-            ->setWallet(collect([self::createWallet(), self::createTransaction()]));
+            ->setWallet(collect([self::createWallet(), self::createWallet()]));
     }
 
     public static function createWallet(): WalletDTO {
         $wallet = new WalletDTO();
 
         return $wallet
-            ->setAddress('AdS7WvzqusoP759qRo6HDmUz2L34u4fMHz')
-            ->setPublicKey('03e7635481b035149829ef74f972e69eca75363bc96e75be57579b0e6fb3f22192')
-            ->setNonce('20')
-            ->setBalance('698316659123862')
+            ->setAddress('AUexKjGtgsSpVzPLs6jNMM6vJ6znEVTQWK')
+            ->setPublicKey('02ff171adaef486b7db9fc160b28433d20cf43163d56fd28fee72145f0d5219a4b')
+            ->setNonce('123102')
+            ->setBalance('691647933769922')
             ->setIsDelegate(false)
             ->setIsResigned(false);
     }
---
+++ tests/Support/WalletsListMock.php
@@ -0,0 +1,39 @@
+<?php
+
+
+namespace Tests\Support;
+
+
+class WalletsListMock
+{
+    public static function jsonResponse()
+    {
+        return json_encode([
+            "meta" => [
+                "count" => 100,
+                "pageCount" => 40366,
+                "totalCount" => 4036503,
+                "next" => "/transactions?transform=true&page=2&limit=100",
+                "previous" => null,
+                "self" => "/transactions?transform=true&page=1&limit=100",
+                "first" => "/transactions?transform=true&page=1&limit=100",
+                "last" => "/transactions?transform=true&page=40366&limit=100"
+            ],
+            "data" => [[
+                "address" => "AUexKjGtgsSpVzPLs6jNMM6vJ6znEVTQWK",
+                "publicKey" => "02ff171adaef486b7db9fc160b28433d20cf43163d56fd28fee72145f0d5219a4b",
+                "nonce" => "123102",
+                "balance" => "691647933769922",
+                "isDelegate" => false,
+                "isResigned" => false
+            ], [
+                "address" => "AUexKjGtgsSpVzPLs6jNMM6vJ6znEVTQWK",
+                "publicKey" => "02ff171adaef486b7db9fc160b28433d20cf43163d56fd28fee72145f0d5219a4b",
+                "nonce" => "123102",
+                "balance" => "691647933769922",
+                "isDelegate" => false,
+                "isResigned" => false
+            ]]
+        ]);
+    }
+}
