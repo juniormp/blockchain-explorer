bfc6619993208cd6432af7d6c90b4f359b6b6b50
Maurício Junior <junior9771@gmail.com>
2020-04-29 15:59:20 -0300

    add list transactions route

--- routes/api.php
+++ routes/api.php
@@ -1,3 +1,4 @@
+
 <?php
 
 use Illuminate\Http\Request;
@@ -16,3 +17,5 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('list-transactions', 'TransactionsController@listTransactions');
