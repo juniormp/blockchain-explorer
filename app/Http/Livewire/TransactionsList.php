7271161d18d4bf230f24c521d5c1331a42e32584
Maurício Junior <junior9771@gmail.com>
2020-05-01 15:07:57 -0300

    get all transactions from controller

--- app/Http/Livewire/TransactionsList.php
+++ app/Http/Livewire/TransactionsList.php
@@ -2,17 +2,25 @@
 
 namespace App\Http\Livewire;
 
+use App\Http\Controllers\TransactionsController;
 use Livewire\Component;
 
 class TransactionsList extends Component
 {
+    public $transactions;
+
+    public function mount(TransactionsController $controller)
+    {
+        $this->transactions = json_decode($controller->listTransactions(), true);
+    }
+
     public function render()
     {
         return view('livewire.transactions-list');
     }
+
     public function click()
     {
         dd(200);
-        $this->emit('clickedEvent');
     }
 }
--- resources/views/livewire/transactions-list.blade.php
+++ resources/views/livewire/transactions-list.blade.php
@@ -1,3 +1,64 @@
 <div>
     <button  wire:click="click"> Click !</button>
 </div>
+
+<div>
+    <table class="border-collapse table-auto w-full whitespace-no-wrap bg-white table-striped relative">
+        <thead>
+        <tr class="text-left">
+            <th class="bg-gray-100 sticky top-0 border-b border-gray-200 px-6 py-2 text-gray-600 font-bold tracking-wider uppercase text-xs">
+                ID
+            </th>
+            <th class="bg-gray-100 sticky top-0 border-b border-gray-200 px-6 py-2 text-gray-600 font-bold tracking-wider uppercase text-xs">
+                Timestamp
+            </th>
+            <th class="bg-gray-100 sticky top-0 border-b border-gray-200 px-6 py-2 text-gray-600 font-bold tracking-wider uppercase text-xs">
+                Sender
+            </th>
+            <th class="bg-gray-100 sticky top-0 border-b border-gray-200 px-6 py-2 text-gray-600 font-bold tracking-wider uppercase text-xs">
+                Recipient
+            </th>
+            </th>
+            <th class="bg-gray-100 sticky top-0 border-b border-gray-200 px-6 py-2 text-gray-600 font-bold tracking-wider uppercase text-xs">
+                Amount
+            </th>
+            </th>
+            <th class="bg-gray-100 sticky top-0 border-b border-gray-200 px-6 py-2 text-gray-600 font-bold tracking-wider uppercase text-xs">
+                Fee
+            </th>
+        </tr>
+        </thead>
+        <tbody>
+        @foreach ($transactions as $transaction)
+            <tr>
+                <td class="border-dashed border-t border-gray-200 userId">
+                    <span class="text-gray-700 px-6 py-3 flex items-center" wire:click="showDetails('{{$transaction['id']}}')" >
+                        {{$transaction['id']}}
+                </span>
+                </td>
+                <td class="border-dashed border-t border-gray-200 firstName">
+                    <span class="text-gray-700 px-6 py-3 flex items-center">{{$transaction['timestamp']}}</span>
+                </td>
+                <td class="border-dashed border-t border-gray-200 lastName">
+                    <span class="text-gray-700 px-6 py-3 flex items-center">{{$transaction['sender']}}</span>
+                </td>
+                <td class="border-dashed border-t border-gray-200 emailAddress">
+                    <span class="text-gray-700 px-6 py-3 flex items-center">{{$transaction['recipient']}}</span>
+                </td>
+                <td class="border-dashed border-t border-gray-200 gender">
+                    <span class="text-gray-700 px-6 py-3 flex items-center">{{$transaction['amount']}}</span>
+                </td>
+                <td class="border-dashed border-t border-gray-200 phoneNumber">
+                    <span class="text-gray-700 px-6 py-3 flex items-center">{{$transaction['fee']}}</span>
+                </td>
+            </tr>
+        @endforeach
+
+        </tbody>
+    </table>
+</div>
+
+
+
+
+
